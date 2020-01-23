f9aaa7b9e81fac12cb7d031122f55fbadd6ddc77
MGSoni <mohitgaurav13@gmail.com>
2020-01-23 20:32:56 +0530

    First Commit

---
+++ project1/src/main/java/com/mohit/ppmtool/Project1ppmApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.ppmtool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project1ppmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project1ppmApplication.class, args);
+	}
+
+}
---
+++ project1/src/test/java/com/mohit/ppmtool/Project1ppmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.ppmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project1ppmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
